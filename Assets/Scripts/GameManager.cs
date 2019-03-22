7e0077be2a1ee96e201a5c2c21e5ce3046bab947
kyechan99 <kyechan99@gmail.com>
2019-03-23 00:05:07 +0900

    Game Manger

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Camera mycam;
+    public GameObject pointer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = mycam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null)
+                {
+                    if (hit.transform.tag.Equals("Tile_Yield"))
+                        pointer.transform.position = new Vector3(hit.transform.position.x, 0.85f, hit.transform.position.z);
+                    else if (hit.transform.tag.Equals("Tile_Water"))
+                        pointer.transform.position = new Vector3(hit.transform.position.x, 0.42f, hit.transform.position.z);
+                }
+            }
+        }
+    }
+}
