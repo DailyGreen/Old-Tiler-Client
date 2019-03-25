267f867b8c9ac2dbe8df037d0913844e4a22e3ae
kyechan99 <kyechan99@gmail.com>
2019-03-25 14:56:19 +0900

    Import Kit Asset

---
+++ Assets/Scripts/Manager/CameraMng.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMng : MonoBehaviour
+{
+    public Camera mycam;
+    public float moveSpeed;
+    public int cameraLevel = 0;     // 멀리 보이는 정도 0, ~ 6 높을수록 멀리 보임
+    [SerializeField]
+    bool scrollUp = false;
+    [SerializeField]
+    bool scrollDown = false;
+    [SerializeField]
+    bool scrollPos = false;
+    Quaternion beforeRot;
+    public Animator camAnim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        keyUpdate();
+    }
+
+    void keyUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -1);
+        }
+        
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && cameraLevel < 10)
+        {
+            //Debug.Log("DOWN");
+            cameraLevel++;
+            //camAnim.speed = 1;
+            //camAnim.SetInteger("cameraLevel", cameraLevel);
+            transform.position += new Vector3(0, 3, 0);
+            mycam.transform.position += new Vector3(0, 0, 5);
+            mycam.transform.Rotate(new Vector3(10, 0, 0));
+            mycam.fieldOfView += 8;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") > 0 && cameraLevel > 0)
+        {
+            //Debug.Log("UP");
+            cameraLevel--;
+            //camAnim.SetInteger("cameraLevel", cameraLevel);
+            //mycam.transform.position += new Vector3(0, -3, 0);
+            transform.position += new Vector3(0, -3, 0);
+            mycam.transform.position += new Vector3(0, 0, -5);
+            mycam.transform.Rotate(new Vector3(-10, 0, 0));
+            mycam.fieldOfView -= 8;
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/GameMng.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameMng : MonoBehaviour
+{
+    public Camera mycam;
+    public GameObject pointer;
+    [SerializeField]
+    GameObject[] pointerChild;
+
+    int[,] tiles = new int[,] {
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+    };
+    //int[,] tiles = new int[,] {
+    //    { 0, 0, 0, 2, -2, -2, -2, -3, -3, 3, 3, 5, 4, 4 },
+    //    { 0, 0, 0, 2, -1, -1, -2, 1, 1, 3, 3, 5, 4, 4 },
+    //    { 0, 0, 0, 2, 1, 1, -1, 1, 1, 3, 3, 5, 4, 4 },
+    //    { 0, 0, 0, 2, 1, 1, 1, 1, 1, 3, 3, 5, 4, 4 },
+    //    { 0, 0, 0, 2, 1, 1, 1, 1, 1, 3, 3, 5, 4, 4 },
+    //    { 0, 0, 0, 2, 1, 1, 1, 1, 1, 3, 3, 5, 4, 4 },
+    //    { 0, 0, 0, 2, 1, 1, 1, 1, 1, 3, 3, 5, 4, 4 },
+    //    { 0, 0, 0, 2, 1, 1, 1, 1, 1, 3, 3, 5, 4, 4 },
+    //    { 0, 0, 0, 2, 1, 1, 1, 1, 1, 3, 3, 5, 4, 4 },
+    //    { 0, 0, 0, 2, 1, 1, 1, 1, 1, 3, 3, 5, 4, 4 },
+    //    { 0, 0, 0, 2, 1, 1, 1, 1, 1, 3, 3, 5, 4, 4 }
+    //};
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+            pointer.SetActive(false);
+
+        if (Input.GetMouseButtonDown(0))
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                Debug.Log("FFFFFFF");
+                RaycastHit hit;
+                Ray ray = mycam.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit, 300.0f))
+                    if (hit.transform != null)
+                        if (hit.transform.tag.Equals("Tile"))
+                        {
+                            int posY = hit.transform.GetComponent<Tile>().posY;
+                            int posX = hit.transform.GetComponent<Tile>().posX;
+                            int code = hit.transform.GetComponent<Tile>().code;
+                            Debug.Log(posY + " : " + posX + " : " + code);
+
+                            if (code >= 0)
+                            {
+                                pointer.transform.position = new Vector3(hit.transform.position.x, 1.6f, hit.transform.position.z);
+                            }
+                            else
+                            {
+                                pointer.transform.position = new Vector3(hit.transform.position.x, 0.21f, hit.transform.position.z);
+                            }
+                            int[,] arr = new int[6, 2] { { 1, 0 }, { 1, 1 }, { 0, 1 }, { -1, 1 }, { -1, 0 }, { 0, -1 } };
+                            if (posY % 2 == 0)
+                                arr = new int[6, 2] { { 1, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 }, { -1, -1 }, { 0, -1 } };
+                            for (int i = 0; i < 6; i++)
+                            {
+                                if (posY + arr[i, 0] >= 0 && posY + arr[i, 0] < 7 &&
+                                    posX + arr[i, 1] >= 0 && posX + arr[i, 1] < 14)
+                                {
+                                    if (code >= 0)
+                                    {
+                                        pointerChild[i].transform.localPosition = new Vector3(
+                                            pointerChild[i].transform.localPosition.x,
+                                            (tiles[posY + arr[i, 0], posX + arr[i, 1]]) >= 0 ? 0 : -0.64f,
+                                            pointerChild[i].transform.localPosition.z);
+                                    }
+                                    else
+                                    {
+                                        pointerChild[i].transform.localPosition = new Vector3(
+                                            pointerChild[i].transform.localPosition.x,
+                                            (tiles[posY + arr[i, 0], posX + arr[i, 1]]) >= 0 ? 0.64f : 0,
+                                            pointerChild[i].transform.localPosition.z);
+                                    }
+                                    pointerChild[i].gameObject.SetActive(true);
+                                }
+                                else
+                                {
+                                    pointerChild[i].gameObject.SetActive(false);
+                                }
+                            }
+                            pointer.SetActive(true);
+                        }
+            }
+    }
+}
